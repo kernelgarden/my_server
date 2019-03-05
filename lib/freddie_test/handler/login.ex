ca12a4fbecc235051796c3f2f4645ff98c52323f
Kernelgarden <kernelgarden@gmail.com>
2019-03-05 16:14:21 +0900

    initial commit

---
+++ lib/freddie_test/handler/login.ex
@@ -0,0 +1,25 @@
+defmodule FreddieTest.Handler.Login do
+
+  def handle({context, msg}) do
+    req_id = msg.id
+
+    user =
+      FreddieTest.Model.User
+      |> FreddieTest.Repo.get(req_id)
+
+    response =
+      case user do
+        nil ->
+          # Todo: add fail process
+          IO.puts("user not exitst")
+          :noop
+        user ->
+          position = FreddieTest.Scheme.Position.new(x: user.current_pos_x, y: user.current_pos_y, z: user.current_pos_z)
+          FreddieTest.Scheme.SC_Login.new(id: user.id, player_name: user.player_name, player_pos: position)
+          |> IO.inspect(label: "[Debug] => User logined: ")
+      end
+
+    Freddie.Session.send(context, response)
+  end
+
+end
---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule FreddieTest.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :freddie_test,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {FreddieTest.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:freddie, path: "../freddie"},
+      {:ecto_sql, "~> 3.0.5"},
+      {:mariaex, "~> 0.9.1"}
+    ]
+  end
+end
